21ca4376e30c46f96dd232cf6438fd5c5baa0e95
yanghyunhwa <yanghh711@naver.com>
2022-08-31 13:22:47 +0900

    API 호출

--- src/pages/ProductAll.jsx
+++ src/pages/ProductAll.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { useEffect } from 'react';
 
 const ProductAll = () => {
+
+	// API 호출 함수
+	const getProducts = async() => {
+		let url = 'http://localhost:3001/products';
+		let response = await fetch(url);
+		let data = await response.json();
+
+		// console.log(data);
+	}
+
+	useEffect(()=>{
+		getProducts();
+	},[])
 	return (
-		<div>ProductAll</div>
+		<div>전체 상품 페이지</div>
 	)
 }
 
