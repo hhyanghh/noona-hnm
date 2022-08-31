4e2038a92c400a5915226359f13a509ae78f1e02
yanghyunhwa <yanghh711@naver.com>
2022-08-31 09:44:02 +0900

    router 설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import ProductAll from './pages/ProductAll';
+import Login from './pages/Login';
+import ProductDetail from './pages/ProductDetail';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Routes>
+        <Route path="/" element={<ProductAll />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+      </Routes>
     </div>
   );
 }
