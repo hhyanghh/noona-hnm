c4e823c2cd4e75a5df26714bc4b79e4193ca114c
yanghyunhwa <yanghh711@naver.com>
2022-08-31 16:19:24 +0900

    login design

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
 	const menuList = [
@@ -13,16 +14,24 @@ const Navbar = () => {
 		"H&M Home",
 		"Sale",
 		"지속가능성"
-	]
+	];
+	const navigate = useNavigate();
+	const goToLogin = () => {
+		navigate('/login');
+	}
+	const goToMain = () => {
+		navigate('/')
+	}
+
 	return (
 		<div>
 			<div>
-				<div className="login-button">
+				<div className="login-button" onClick={goToLogin}>
 					<FontAwesomeIcon icon={faUser} className="mr-4" />
 					<div>로그인</div>
 				</div>
 			</div>
-			<div className="flex-center">
+			<div className="flex-center" onClick={goToMain}>
 				<img width={100} src="https://logos-world.net/wp-content/uploads/2020/04/HM-Logo.png" alt="H&M" />
 			</div>
 			<div className="menu-area">
